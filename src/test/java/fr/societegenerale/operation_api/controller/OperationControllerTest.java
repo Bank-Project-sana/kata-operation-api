6472f8fd759ba86e793c57b040764eaf375b6ecb
sana_benahmed <sana.benahmed@enis.tn>
2022-08-26 11:05:13 +0100

    US 3: consult user operations TU

--- src/test/java/fr/societegenerale/operation_api/controller/OperationControllerTest.java
+++ src/test/java/fr/societegenerale/operation_api/controller/OperationControllerTest.java
@@ -1,13 +1,31 @@
 package fr.societegenerale.operation_api.controller;
 
+import fr.societegenerale.controller.OperationController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 
 
 public class OperationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+@MockBean
+    OperationController operationController;
+
+    @Test
+    public void getAllOperationByClientAccountIdTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/bank/v1/operations/client-account").param("id","1")).andExpect(status().isOk());
+    }
+
 
 
 }
--- src/test/java/fr/societegenerale/operation_api/service/OperationServiceTest.java
+++ src/test/java/fr/societegenerale/operation_api/service/OperationServiceTest.java
@@ -1,17 +1,32 @@
 package fr.societegenerale.operation_api.service;
 
+import fr.societegenerale.model.OperationEntity;
+import fr.societegenerale.model.OperationTypeEnum;
+import fr.societegenerale.repository.OperationRepository;
+import fr.societegenerale.service.ClientAccountService;
 import fr.societegenerale.service.OperationService;
 import fr.societegenerale.service.impl.OperationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.TestContextManager;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 
 public class OperationServiceTest {
     @Autowired
     private OperationService operationService;
+
+    @MockBean
+    private OperationRepository operationRepository;
 
 
 
@@ -28,5 +43,18 @@ public class OperationServiceTest {
         TestContextManager testContextManager = new TestContextManager(getClass());
         testContextManager.prepareTestInstance(this);
     }
+
+    @Test
+    void getAllOperationByClientAccountIdTest() throws Exception {
+        Set<OperationEntity> operations = new HashSet<>();
+        operations.add(OperationEntity.builder().id(1L).type(OperationTypeEnum.WITHDRAWAL).build());
+        operations.add(OperationEntity.builder().id(2L).type(OperationTypeEnum.WITHDRAWAL).build());
+
+        Mockito.when(operationRepository.findByAccountIdOrderByDateDesc(Mockito.any())).thenReturn(operations);
+
+        assertEquals(operations.size(), operationService.getAllOperationByClientAccountId(1L).size());
+
+    }
+
 
 }
